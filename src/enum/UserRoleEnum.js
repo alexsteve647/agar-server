acebcffe3d574e6e7f33feee6b886c150c050f24
alexsteve647 <56225128+alexsteve647@users.noreply.github.com>
2021-12-12 19:00:44 +0200

    Add files via upload

---
+++ src/enum/UserRoleEnum.js
@@ -0,0 +1,13 @@
+﻿function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value: value,
+        enumerable: true,
+        writable: true,
+        configurable: true
+    });
+}
+
+define("GUEST", 0);
+define("USER", 1);
+define("MODER", 2);
+define("ADMIN", 4);
---
+++ src/packet/ServerStat.js
@@ -0,0 +1,45 @@
+﻿// Import
+var BinaryWriter = require("./BinaryWriter");
+var Cell = require("../entity/Cell");
+
+function ServerStat(playerTracker) {
+    this.playerTracker = playerTracker;
+};
+
+module.exports = ServerStat;
+
+ServerStat.prototype.build = function (protocol) {
+    var gameServer = this.playerTracker.gameServer;
+    // Get server statistics
+    var totalPlayers = 0;
+    var alivePlayers = 0;
+    var spectPlayers = 0;
+    var eatenpellets = gameServer.currentFood;
+    for (var i = 0; i < gameServer.clients.length; i++) {
+        var socket = gameServer.clients[i];
+        if (socket == null || !socket.isConnected)
+            continue;
+        totalPlayers++;
+        if (socket.playerTracker.cells.length > 0)
+            alivePlayers++;
+        else
+            spectPlayers++;
+    }
+    var obj = {
+        'name': gameServer.config.serverName,
+        'mode': gameServer.gameMode.name,
+        'uptime': process.uptime() >>> 0,
+        'update': gameServer.updateTimeAvg.toFixed(3),
+        'playersTotal': totalPlayers,
+        'playersAlive': alivePlayers,
+        'playersSpect': spectPlayers,
+        'playersLimit': gameServer.config.serverMaxConnections,
+        'pelletsEaten': eatenpellets
+    };
+    var json = JSON.stringify(obj);
+    // Serialize
+    var writer = new BinaryWriter();
+    writer.writeUInt8(254);             // Message Id
+    writer.writeStringZeroUtf8(json);   // JSON
+    return writer.toBuffer();
+};
